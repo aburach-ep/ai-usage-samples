9a786c267d909b7ba9dea5510c0c695a9e7b8658
Andrii Burachynskyi <andrii_burachynskyi@epam.com>
2025-04-17 18:46:06 +0300

    added sample ai-usage-samples project. Added BubbleSortSample

---
+++ src/main/java/com/ai/usage/AiUsageSamplesApplication.java
@@ -0,0 +1,13 @@
+package com.ai.usage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiUsageSamplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiUsageSamplesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ai/usage/samples/AiUsageSamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ai.usage.samples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiUsageSamplesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
